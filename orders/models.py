c65d15863b33875c1455f7896d93eeb7aba41206
Kama <komaliyeva@gmail.com>
2022-05-30 20:52:03 +0600

    not errors

--- orders/models.py
+++ orders/models.py
@@ -12,7 +12,7 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
-    time = models.TimeField()
+    date_time = models.DateTimeField(null=True)
 
     class Meta:
         ordering = ('-created',)
--- orders/views.py
+++ orders/views.py
@@ -1,36 +1,28 @@
-from django.contrib import messages
 from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
-from django.core.mail import send_mail
 
 
 def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
-        message_name = request.POST['first_name']
-        message_email = request.POST['email']
-        message = request.POST['address']
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
             for item in cart:
-                send_mail(message_name, message, message_email,
-                                 ['komaliyeva@gmail.com'], fail_silently=False)
+
                 OrderItem.objects.create(order=order,
                                          product=item['product'],
                                          price=item['price'],
                                          quantity=item['quantity'])
 
             cart.clear()
-            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
-        else:
-            form = OrderCreateForm()
+        form = OrderCreateForm()
         return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
     return render(request, 'orders/order/create.html', {'cart': cart})
 
